f276ea51f951867627b90f0325584cd6b01c4451
ZVLU <zvluwork@gmail.com>
2020-06-08 16:31:16 -0400

    main index.js

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Testimonials extends Component {
+  render() {
+
+    if(this.props.data){
+      var testimonials = this.props.data.testimonials.map(function(testimonials){
+        return  <li key={testimonials.user}>
+            
+               <p>{testimonials.text}</p>
+               <cite>{testimonials.user}</cite>
+            
+         </li>
+      })
+    }
+
+    return (
+      <section id="testimonials">
+      <div className="text-container">
+         <div className="row">
+
+            <div className="two columns header-col">
+               {/* <h1><span>Why Tech?</span></h1> */}
+            </div>
+
+            <div className="ten columns flex-container">
+                  <ul className="slides">
+                      {testimonials}
+                  </ul>
+               </div>
+            </div>
+         </div>
+   </section>
+    );
+  }
+}
+
+export default Testimonials;
